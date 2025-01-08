9227f3f3988d39725ac79de7d9b89b73cc12ea14
Abdul Aziz <aziz@marensolutions.com>
2025-01-08 23:13:16 +0530

    update formula

--- kvstore.rb
+++ kvstore.rb
@@ -3,14 +3,14 @@ class Kvstore < Formula
     homepage "https://github.com/codemeall/cligo"
     url "https://github.com/codemeall/cligo/releases/download/v1.0.0/kvstore-1.0.0.tar.gz"
     version "1.0.0"
-    sha256 "6a68fed4465a312dc4f577d0c5bdaf15dd2b1c14aea460432bf06024177c4e20" # You'll need to calculate this
+    sha256 "6a68fed4465a312dc4f577d0c5bdaf15dd2b1c14aea460432bf06024177c4e20" # Add your tar.gz SHA256 here
   
     def install
-      system "make" # Adjust based on your build system
-      bin.install "kvstore" # Adjust binary name if different
+      # Simply install the binary
+      bin.install "kvstore"
     end
   
     test do
-      system "#{bin}/kvstore", "--version"
+      system "#{bin}/kvstore", "version"
     end
   end
